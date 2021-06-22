398ab4ff51454a2f393a3a93d35774d412c85881
Gheorghita Cristea <gcristea73@gmail.com>
2021-06-22 14:59:24 +0100

    add tall screen view

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <meta charset="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Reading view</title>
       </Head>
       <main >
